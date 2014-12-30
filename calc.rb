3dcc5da316a87c690f0e37265f343284bc999c10
bklynate <nathanielcfa@gmail.com>
2014-12-29 21:31:45 -0500

    Refractored the code - more DRY. Added methods to vaildate numbers for equations and to validate player choice

--- calc.rb
+++ calc.rb
@@ -1,7 +1,7 @@
 # Calc.rb - A Simple Calculator Script
 
-def num_check(num)
-  num.to_i.to_s == num || num.to_f.to_s == num
+def number_check(number)
+  number.to_i.to_s == number || number.to_f.to_s == num
 end
 
 def prompt(words)
@@ -10,56 +10,46 @@ end
 
 def user_input
   begin
-    num = gets.chomp
-    puts "That's not a valid number, please try again" unless num_check(num)
-  end until num_check(num)
-  return num
+    number = gets.chomp
+    prompt "That's not a valid number, please try again" unless number_check(number)
+  end until number_check(number)
+  return number
+end
+
+def valid_choice
+  begin
+    choice_list = ["1","2","3","4"]
+    choices = gets.chomp
+    puts  "That's not a valid choice, please try again" unless choice_list.include?(choices)
+  end until choice_list.include?(choices)
+  return choices
 end
 
 prompt "Calculator Initalized: 100%"
 
-begin
+begin 
   prompt "Press: 1) Addition  2)Substraction  3)Multiply  4) Division" 
-  choice = gets.chomp
+  choice = valid_choice
+
+  prompt "1st input: "
+  num1 = user_input
+  prompt "2nd input: "
+  num2 = user_input 
+  
   if choice == "1"
-    prompt "1st input: "
-    num1 = user_input
-    prompt "2nd input: "
-    num2 = user_input
-    result = num1.to_i + num2.to_i
-    prompt "The result is #{result}"
-    prompt "Again[y/n]?"
-    decision = gets.chomp.downcase
+    result = num1.to_i + num2.to_i  
+
   elsif choice == "2"
-    prompt "1st input: "
-    num1 = user_input
-    prompt "2nd input: "
-    num2 = user_input
     result = num1.to_i - num2.to_i
-    prompt "The result is #{result}"
-    prompt "Again[y/n]?"
-    decision = gets.chomp.downcase
+
   elsif choice == "3"
-    prompt "1st input: "
-    num1 = user_input
-    prompt "2nd input: "
-    num2 = user_input
     result = num1.to_i * num2.to_i
-    prompt "The result is #{result}"
-    prompt "Again[y/n]?"
-    decision = gets.chomp.downcase
+
   elsif choice == "4"
-    prompt "1st input: "
-    num1 = user_input
-    prompt "2nd input: "
-    num2 = user_input
     result = num1.to_i / num2.to_f
-    prompt "The result is #{result}"
-    prompt "Again[y/n]?"
-    decision = gets.chomp.downcase
-  else
-    prompt "That is not a valid choice"
-    prompt "Choose again?[y/n] "
-    decision = gets.chomp.downcase
   end
+
+  prompt "The result is #{result}" if result
+  prompt "Try Again? [y/n]"
+  decision = gets.chomp.downcase
 end while decision == "y"
