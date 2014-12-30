2ece79edcc3356df085f87da09d5de0b8eb48a63
bklynate <nathanielcfa@gmail.com>
2014-12-29 21:03:30 -0500

    Added a method to validate the player choice

--- refactored_calc.rb
+++ refactored_calc.rb
@@ -11,43 +11,45 @@ end
 def user_input
   begin
     num = gets.chomp
-    puts "That's not a valid number, please try again" unless num_check(num)
+    prompt "That's not a valid number, please try again" unless num_check(num)
   end until num_check(num)
   return num
 end
 
+def valid_choice
+  begin
+    choice_list = ["1","2","3","4"]
+    choices = gets.chomp
+    puts  "That's not a valid choice, please try again" unless choice_list.include?(choices)
+  end until choice_list.include?(choices)
+  return choices
+end
+
 prompt "Calculator Initalized: 100%"
 
-
 begin 
-prompt "Press: 1) Addition  2)Substraction  3)Multiply  4) Division" 
-choice = gets.chomp
+  prompt "Press: 1) Addition  2)Substraction  3)Multiply  4) Division" 
+  choice = valid_choice
 
-prompt "1st input: "
-num1 = user_input
-prompt "2nd input: "
-num2 = user_input 
+  prompt "1st input: "
+  num1 = user_input
+  prompt "2nd input: "
+  num2 = user_input 
   
   if choice == "1"
-    result = num1.to_i + num2.to_i
-    prompt "The result is #{result}"
+    result = num1.to_i + num2.to_i  
 
   elsif choice == "2"
     result = num1.to_i - num2.to_i
-    prompt "The result is #{result}"
 
   elsif choice == "3"
     result = num1.to_i * num2.to_i
-    prompt "The result is #{result}"
 
   elsif choice == "4"
     result = num1.to_i / num2.to_f
-    prompt "The result is #{result}"
-  
-  else
-    prompt "That is not a valid choice"
   end
-
+  
+  prompt "The result is #{result}" if result
   prompt "Try Again? [y/n]"
   decision = gets.chomp.downcase
 end while decision == "y"
